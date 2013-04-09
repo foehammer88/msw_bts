8f042581eb82ddd498d35699e6648fcaaa49f9bb
Patrick Ganson <foehammer88@gmail.com>
2013-04-09 17:28:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MswBts::Application.config.secret_token = '18e701c0bdd2853297bbeff803d686109d96478adf4ceb5ee2d88768b3f3fe53e8f947b4acf841fa79091fa70dea3c1fad89ccf6d2efbe0cba332598686c5c3b'
